cf4fe73a951c9a6265174c272174bdc5123bc795
user <ccasbre27@hotmail.com>
2015-12-26 23:57:04 -0600

    1 - Se agregan imágenes. 2 - Se agrega TableViewController, Modelo y Vista para las opciones

---
+++ Carriot/OptionTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  OptionTableViewCell.h
+//  Carriot
+//
+//  Created by user on 12/26/15.
+//  Copyright © 2015 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OptionTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *imgLogo;
+@property (weak, nonatomic) IBOutlet UILabel *lblTitle;
+
+@end
---
+++ Carriot/OptionTableViewController.m
@@ -0,0 +1,95 @@
+//
+//  OptionTableViewController.m
+//  Carriot
+//
+//  Created by user on 12/26/15.
+//  Copyright © 2015 user. All rights reserved.
+//
+
+#import "OptionTableViewController.h"
+#import "Option.h"
+#import "OptionTableViewCell.h"
+
+@interface OptionTableViewController ()
+{
+    NSMutableArray * optionsArray;
+}
+@end
+
+@implementation OptionTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // cargamos los datos en un array
+    [self loadData];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void) loadData
+{
+    optionsArray = [[NSMutableArray alloc]init];
+    
+    [optionsArray addObject:[[Option alloc] initWithTitle:@"Historia" PictureURL:@"history.png"]];
+    [optionsArray addObject:[[Option alloc] initWithTitle:@"Rutas" PictureURL:@"routes.png"]];
+    [optionsArray addObject:[[Option alloc] initWithTitle:@"Sitios de interés" PictureURL:@"building.png"]];
+    [optionsArray addObject:[[Option alloc] initWithTitle:@"Eventos" PictureURL:@"event.png"]];
+    [optionsArray addObject:[[Option alloc] initWithTitle:@"Religión" PictureURL:@"church.png"]];
+    [optionsArray addObject:[[Option alloc] initWithTitle:@"Facebook" PictureURL:@"facebook.png"]];
+    [optionsArray addObject:[[Option alloc] initWithTitle:@"Sitio Web" PictureURL:@"website.png"]];
+    [optionsArray addObject:[[Option alloc] initWithTitle:@"Contacto" PictureURL:@"message.png"]];
+}
+
+
+#pragma mark - Table view data source
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return optionsArray.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 10;
+}
+
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIView *v = [UIView new];
+    [v setBackgroundColor:[UIColor clearColor]];
+    return v;
+}
+
+
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString * cellIdentifier = @"optionTableViewCell";
+    
+    OptionTableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    
+    long row = [indexPath section];
+    
+    Option *option = [optionsArray objectAtIndex:row];
+    
+    [cell.lblTitle setText:option.title];
+    [cell.imgLogo setImage:[UIImage imageNamed:option.image]];
+    
+    
+    [cell.layer setCornerRadius:35.0f];
+    
+    return cell;
+}
+
+@end
